4261ba9a252d9010ac8c50e3d490649a498c00d1
Emilse Arias <emilsearias@gmail.com>
2022-08-18 09:35:46 -0300

    agregamos el maquetado del form

--- src/components/views/producto/CrearProducto.js
+++ src/components/views/producto/CrearProducto.js
@@ -7,15 +7,29 @@ const CrearProducto = () => {
       <h1 className="display-4 mt-5">Nuevo producto</h1>
       <hr />
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+        <Form.Group className="mb-3" controlId="formNombreProdcuto">
+          <Form.Label>Nombre producto*</Form.Label>
+          <Form.Control type="text" placeholder="Ej: Cafe" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formNuevo">
-          <Form.Label>Email sjdkfhskdfh</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+        <Form.Group className="mb-3" controlId="formPrecio">
+          <Form.Label>Precio*</Form.Label>
+          <Form.Control type="text" placeholder="Ej: 50" />
         </Form.Group>
-
+        <Form.Group className="mb-3" controlId="formPrecio">
+          <Form.Label>Imagen URL*</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ej: https://www.pexels.com/es-es/foto/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPrecio">
+          <Form.Label>Categoria*</Form.Label>
+          <Form.Select>
+            <option value="">Seleccione una opcion</option>
+            <option value="">Una</option>
+            <option value="">Dos</option>
+          </Form.Select>
+        </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>
