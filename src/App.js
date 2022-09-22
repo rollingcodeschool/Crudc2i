dfba1032c7af1b3aa530a20fddc55dd0577ff064
Emilse Arias <emilsearias@gmail.com>
2022-09-22 12:43:58 -0300

    agregamos rutas protegidas al proyecto

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import DetalleProducto from './components/views/DetalleProducto';
 import Login from './components/views/Login';
 import Registro from './components/views/Registro';
 import { useState } from 'react';
+import RutaProtegida from './components/routes/RutaProtegida';
 
 function App() {
   const [usuarioLogueado, setUsuarioLogueado] = useState({})
@@ -24,10 +25,17 @@ function App() {
           <Route exact path='/' element={<Home></Home>} ></Route>
           <Route exact path='/login' element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>} ></Route>
           <Route exact path='/registro' element={<Registro></Registro>} ></Route>
-          <Route exact path='/administrar' element={<AdministrarProductos></AdministrarProductos>} ></Route>
-          <Route exact path='/administrar/crear' element={<CrearProducto></CrearProducto>} ></Route>
-          <Route exact path='/administrar/editar/:id' element={<EditarProducto></EditarProducto>} ></Route>
           <Route exact path='/administrar/producto/detalle/:id' element={<DetalleProducto></DetalleProducto>}></Route>
+         <Route path='/*' element={
+          <RutaProtegida>
+            <Routes>
+              <Route exact path='/administrar' element={<AdministrarProductos></AdministrarProductos>} ></Route>
+              <Route exact path='/administrar/crear' element={<CrearProducto></CrearProducto>} ></Route>
+              <Route exact path='/administrar/editar/:id' element={<EditarProducto></EditarProducto>} ></Route>
+            </Routes>
+          </RutaProtegida>
+         }>
+         </Route>
           <Route path='*' element={<Error404></Error404>}></Route>
         </Routes>
         <Footer></Footer>
